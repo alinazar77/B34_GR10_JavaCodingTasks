d62dac03bd1591c08d18ad823c6a585140ae36a4
IdeenNova <ideen@cydeo.com>
2024-05-22 14:30:38 -0500

    add week1 tasks

--- src/mentor/week1/DivideWithoutOperator.java
+++ src/mentor/week1/DivideWithoutOperator.java
@@ -1,4 +1,9 @@
 package mentor.week1;
 
 public class DivideWithoutOperator {
+     /*
+Write a method that can divide two numbers without using division operator
+
+ */
+
 }
--- src/mentor/week1/EvenOdd.java
+++ src/mentor/week1/EvenOdd.java
@@ -1,4 +1,11 @@
 package mentor.week1;
 
 public class EvenOdd {
+     /*
+Write a method which can identifies given number is even or odd.
+ Ex:
+     idenitify(5) -> "Odd"
+     idenitify(6) -> "Even"
+ */
+
 }
