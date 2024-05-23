2a059981676c46ea5f75b147e4708c1226f802b6
IdeenNova <ideen@cydeo.com>
2024-05-23 09:21:21 -0500

    add output directory

--- src/mentor/week1/EvenOdd.java
+++ src/mentor/week1/EvenOdd.java
@@ -7,5 +7,7 @@ Write a method which can identifies given number is even or odd.
      idenitify(5) -> "Odd"
      idenitify(6) -> "Even"
  */
+     public static void main(String[] args) {
 
+     }
 }
