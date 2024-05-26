c44e10c225c94a7bba79e5665fb144f58d2e1b9a
zaito <zaitoova1205@gmail.com>
2024-05-26 15:28:42 -0500

    Added FINRA method

---
+++ src/tahmina/week1/EvenOdd.java
@@ -0,0 +1,17 @@
+package tahmina.week1;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
+
+   //Write a method which can identifies given number is even or odd.
+  //Ex:
+     //idenitify(5) -> "Odd"
+    //idenitify(6) -> "Even"
