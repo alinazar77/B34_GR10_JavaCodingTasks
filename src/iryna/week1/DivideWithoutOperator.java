34d3e9de35aca1c623813414fcaf14d24bfe489e
ibobechko <bobechkoira@gmail.com>
2024-05-28 22:41:54 -0400

    updated DivideWithoutOperator

--- src/iryna/week1/DivideWithoutOperator.java
+++ src/iryna/week1/DivideWithoutOperator.java
@@ -7,15 +7,15 @@ Write a method that can divide two numbers without using division operator
 */
     public static void main(String[] args) {
 
-        divisionWithoutOperator(10,2);
+        System.out.println(divisionWithoutOperator(10, 2));
     }
-    public static void divisionWithoutOperator (int num1, int num2) {
+    public static Integer divisionWithoutOperator (int num1, int num2) {
 
         int count = 0;
         while (num1 >= num2){
          num1 -= num2;
             count++;
         }
-        System.out.println(count);
+        return count;
     }
 }
