a92070df4f8d589b0c81c414494de309dbec3112
IdeenNova <ideen@cydeo.com>
2024-05-29 12:48:35 -0500

    Revert "add week1 solutions and week2 tasks"
    This reverts commit a66556e2af017b37d1dddfc4a630da6a2b6125fe.

--- src/mentor/week1/EvenOdd.java
+++ src/mentor/week1/EvenOdd.java
@@ -8,16 +8,6 @@ Write a method which can identifies given number is even or odd.
      idenitify(6) -> "Even"
  */
      public static void main(String[] args) {
-          //Write  a method which can identifies given number is even or odd
 
-          identifyOddEven(5);
-          identifyOddEven(6);
-
-     }
-     public  static  void  identifyOddEven( int  n ) {
-          if(n%2==0){
-               System.out.println("even");}
-          else{
-               System.out.println("odd");}
      }
 }
